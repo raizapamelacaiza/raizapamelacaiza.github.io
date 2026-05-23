a209f20401926476ba358b83ce343b30c21e817b
raizapamelacaiza <raizapamelacaiza@gmail.com>
2026-05-23 22:21:56 +0000

    Deploying to gh-pages from @ raizapamelacaiza/raizapamelacaiza.github.io@40d5ef0ecfb95b5bb30f2d0f9b97d24aae9e0dfa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,16 +398,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-presentation-at-the-eleventh-conference-of-the-society-for-the-study-of-economic-inequality-ecineq-at-the-world-bank-washington-dc",
+          title: 'Presentation at the Eleventh Conference of the Society for the Study of Economic...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
